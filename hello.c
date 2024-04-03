0bcb6260ab5827532cad19cf5e9853b0c490dab6
Pseudo_Supreme <gadgetguy49@gmail.com>
2024-04-03 12:47:54 -0400

    adding comments now

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main()
-{
+{ // this is gigachad code right here
     printf("hello world\n");
     printf("I lke pizza it's really good!");
     return 0;
-}
+} // gigachad as heck
